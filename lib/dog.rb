165e1156454b2f3154a95b167e9c3e8429572750
Robert Clarke <r.jason.clarke@gmail.com>
2020-12-23 03:00:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+class Dog
+
+	@@all = []
+	attr_accessor :name
+
+	def initialize(name)
+		@name = name
+		@@all << self
+	end
+
+  def self.all
+    puts @@all
+
+	def self.clear_all
+		@@all.clear
+
+	end
+
+	def self.print_all
+		@@all.each do |dog|
+			puts dog.name
+		end
+	end
+end
+
+pluto = Dog.new("Pluto")
+fido = Dog.new("Fido")
+maddy = Dog.new("Maddy")
